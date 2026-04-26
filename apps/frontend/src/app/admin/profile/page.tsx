a8aaa5774de4dbb090eed28abe1c5838d6793d01
Methexx <methum.edu@gmail.com>
2026-04-27 02:15:27 +0530

    chore: husky linitng isssue fix

--- apps/frontend/src/app/admin/profile/page.tsx
+++ apps/frontend/src/app/admin/profile/page.tsx
@@ -58,9 +58,7 @@ export default function ProfilePage() {
 
       // eslint-disable-next-line react-hooks/set-state-in-effect
       setFormData(newData);
-      // eslint-disable-next-line react-hooks/set-state-in-effect
       setInitialData(newData);
-      // eslint-disable-next-line react-hooks/set-state-in-effect
       setProfileImage(user.avatar_url || null);
     }
   }, [user]);
