9a0a4f49c0198c8e69b9955054beb29e02c7c294
Methexx <methum.edu@gmail.com>
2026-05-20 04:14:59 +0530

    fix: app run failed issue fix

--- apps/flutter/lib/core/api/api_config.dart
+++ apps/flutter/lib/core/api/api_config.dart
@@ -1,7 +1,6 @@
 class ApiConfig {
-  // Android emulator uses 10.0.2.2 to access host machine localhost.
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:5000',
+    defaultValue: '',
   );
 }
